b4d5c383646082cd1406bf69300ad2f89e9a15f1
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-03-04 20:16:21 +0100

    Final

---
+++ src/routes/subject.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+import { delete_Subject,get_Subject,get_Subjects,post_Subject, update_Subject } from "../controllers/subject";
+
+const router=Router();
+
+router.get("/",get_Subjects);
+router.get("/:idSubject",get_Subject);
+router.post("/",post_Subject);
+router.put("/:idSubject",update_Subject);
+router.delete("/:idSubject",delete_Subject);
+export{router};
